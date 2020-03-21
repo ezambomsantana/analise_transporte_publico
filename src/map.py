974fc168c2dcaf4dd4e31aed028efb2e29ba4dee
Eduardo Santana <ezambomsantana@gmail.com>
2020-03-21 18:24:54 -0300

    distance

--- src/map.py
+++ src/map.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import unidecode
 import math
 import utm
+import geopy.distance
 from shapely.geometry import shape, LineString, Polygon
 import matplotlib as mpl
 
@@ -44,7 +45,6 @@ with open(csv_file, mode='r') as infile:
     mydict_pop = {rows[1]:rows[2] for rows in reader}
 
 distritos_pop = pd.DataFrame.from_dict(mydict_pop, orient='index', columns=[ 'Populacao'])
-print(distritos_pop)
 
 distritos_pop['Populacao'] = distritos_pop['Populacao'].astype(float)
 
@@ -52,6 +52,11 @@ data17['NOME_O'] = data17['ZONA_O'].apply(lambda x: '' if pd.isnull(x) else mydi
 data17['NOME_D'] = data17['ZONA_D'].apply(lambda x: '' if pd.isnull(x) else mydict[x])
 
 data17 = data17[data17['MOTIVO_D'].isin([1,2,3])] 
+
+def calculate_distance(row):  
+    origin = utm.to_latlon(row['CO_O_X'],row['CO_O_Y'], 23, 'K')
+    dest = utm.to_latlon(row['CO_D_X'],row['CO_D_Y'], 23, 'K')
+    return geopy.distance.geodesic(origin, dest).meters
 
 def load_districts(vehicle, orde, data):
 
@@ -61,11 +66,17 @@ def load_districts(vehicle, orde, data):
         for v in vehicles:
             vehicles_int.append(int(v))
         data17_copy = data[data['MODOPRIN'].isin(vehicles_int)] 
+
+    data17_copy['DISTANCE'] = data17_copy.apply(lambda x: calculate_distance(x), axis=1)
+    data17_copy['DISTANCE'] = data17_copy['DISTANCE'] / 1000
     
     data_mp2 = data17_copy[[orde,  'FE_VIA']].groupby([orde]).sum().sort_values(by=['FE_VIA']).reset_index()
     data_mp2 = data_mp2.set_index(orde)
 
-    df = mapa.set_index('NomeDistri').join(data_mp2).join(distritos_pop)
+    data_distance = data17_copy[[orde,  'DISTANCE']].groupby([orde]).mean().sort_values(by=['DISTANCE']).reset_index()
+    data_distance = data_distance.set_index(orde)
+
+    df = mapa.set_index('NomeDistri').join(data_mp2).join(distritos_pop).join(data_distance)
     df = df.reset_index()
 
     return df
@@ -109,7 +120,13 @@ for x in range(0, 4):
     plt.axis('off')
     plt.savefig(folder_images_maps + nome + '_quantidade_transporte_densidade_populacao.png', bbox_inches='tight', pad_inches=0.0)
 
-    teste2[['NomeDistri','densidade_area', 'FE_VIA','area','Populacao','densidade_pop']].to_csv('../generated_data/' + nome + '_transporte.csv')
+    fig, ax = plt.subplots(1, 1)
+    teste2.plot(column='DISTANCE', ax=ax, legend=True, cmap=cmap)
+
+    plt.axis('off')
+    plt.savefig(folder_images_maps + nome + '_distancia.png', bbox_inches='tight', pad_inches=0.0)
+
+    teste2[['NomeDistri','densidade_area', 'FE_VIA','area','Populacao','densidade_pop', 'DISTANCE']].to_csv('../generated_data/' + nome + '_transporte.csv')
 
 
 
@@ -151,8 +168,10 @@ for x in range(0, 4):
     plt.savefig(folder_idosos_images_maps + nome + '_quantidade_transporte_densidade_populacao.png', bbox_inches='tight', pad_inches=0.0)
 
 
-    teste2[['NomeDistri','densidade_area', 'FE_VIA','area','Populacao','densidade_pop']].to_csv('../idoso_generated_data/' + nome + '_transporte.csv')
+    fig, ax = plt.subplots(1, 1)
+    teste2.plot(column='DISTANCE', ax=ax, legend=True, cmap=cmap)
 
+    plt.axis('off')
+    plt.savefig(folder_idosos_images_maps + nome + '_distancia_idoso.png', bbox_inches='tight', pad_inches=0.0)
 
-
-
+    teste2[['NomeDistri','densidade_area', 'FE_VIA','area','Populacao','densidade_pop','DISTANCE']].to_csv('../idoso_generated_data/' + nome + '_transporte.csv')
